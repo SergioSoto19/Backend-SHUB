7fc7c167f5e2406fd0040c1c708fc482baee14c4
SergioSoto19 <ser192000gio@gmail.com>
2025-01-11 17:51:57 -0500

    funcionalidad de registro y obtención de usuarios; encriptación de contraseñas con bcrypt

---
+++ src/controllers/userController.js
@@ -0,0 +1,44 @@
+import { PrismaClient } from '@prisma/client';
+import { encryptPassword } from '../utils/authUtils.js';
+
+const prisma = new PrismaClient();
+
+const getUsers = async (req, res) => {
+    try {
+        const users = await prisma.user.findMany();
+        res.json(users);
+    } catch (error) {
+        console.error('Error al obtener los usuarios:', error);
+        res.status(500).json({ message: 'Error al obtener los usuarios' });
+    }
+};
+
+const registerUser = async (req, res) => {
+    const { email, password, role } = req.body;
+
+    try {
+        const existingUser = await prisma.user.findFirst({ where: { email } });
+
+        if (existingUser) {
+            res.status(400).json({ message: 'El correo ya está registrado' });
+        } else {
+            const passwordHash = await encryptPassword(password);
+
+            const newUser = await prisma.user.create({
+                data: {
+                    email,
+                    password: passwordHash, 
+                    role
+                }
+            });
+            res.status(200).json({ message: 'Usuario registrado con éxito' });
+        }
+
+    } catch (error) {
+        console.error('Error al registrar el usuario:', error);
+        res.status(500).json({ message: 'Error al registrar el usuario' });
+    }
+};
+
+
+export { getUsers, registerUser };
--- src/routers/routeUser.js
+++ src/routers/routeUser.js
@@ -1,11 +1,9 @@
 import express from 'express';
-
+import {getUsers,registerUser} from '../controllers/userController.js';
 
 const router = express.Router();
 
-router.get('/users', (req, res) => {
-    res.send('Lista de usuarios');
-  });
-
+router.get('/users', getUsers);
+router.post('/register', registerUser);
 
 export default router;
